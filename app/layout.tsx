41983673f851b665af6217df96cef4f3ec19848e
Ruben Giraldo <ru.giraldo@gmail.com>
2024-07-25 04:16:42 -0500

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,15 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import type { Metadata } from "next";
+import { Poppins as FontSans } from "next/font/google";
+import { cn } from '@/lib/utils'
+
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  weight: ['400', '600'],
+  variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +23,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        fontSans.variable
+      )}
+      >{children}</body>
     </html>
   );
 }
